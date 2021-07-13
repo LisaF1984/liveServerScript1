aaf51ebda2cdd44ce0d4d82a62567866f8ce7e12
LisaF1984 <lisafran1984g@gmail.com>
2021-07-13 16:36:31 +0100

    README.md

---
+++ script.js
@@ -0,0 +1,165 @@
+ "use strict"
+
+//  //cameron examples
+//  let text = 'this is a message';
+//  console.log(text);
+
+// console.info(text);
+
+// console.warn(text);
+
+// console.error(text);
+
+
+// let aBoolean = true;
+// let aNumber = 3.2;
+// let aString = 'text';
+// let aBigInt = 99999999999n;
+// let aBrokenBigInt = 9999999999;
+// let aNull = null;
+// let anUndefined;
+
+// console.log(aBigInt);
+// console.log(aBrokenBigInt);
+// console.log(anUndefined);
+// console.log(typeof(anUndefined));
+// console.log(aNull);
+// console.log(typeof(aNull));
+
+
+// //qa excercises
+// let totalMoney = 4000;
+// let moneyPaidSoFar = 2348;
+// let totalLeftToPay = totalMoney - moneyPaidSoFar;
+
+// console.log(totalLeftToPay);
+
+// function sayHello(){
+// return ('Someone says Hello')
+
+// }
+// console.log(sayHello);
+
+// for(let i = 0; i<10; i++){
+//     console.log('i='+i); 
+// }
+
+// // let a = false;
+
+// // do {
+// //     console.log(`i am batman!`);
+// // } while(a);
+
+// //Cameron switch e.g 
+// let num = 4;
+
+// switch(num) {
+//     case 0:
+//         console.log(`zero is zero`);
+//         break;
+//     case 1:
+//         console.log(`we're at one`);
+//     case 4:
+//         console.log(`now it's four`);
+//     case 10:
+//         console.log(`we've made it to ten!`)
+//         break;
+// }
+// //QA excercise 
+// //1
+// let a = 100;
+// while (a <= 200) {
+//   a++;
+//   console.log(`a = ${a}`);
+
+//   //2
+
+// let a =100;
+// while(a<=200) {
+//     if(a%2==0) {
+//         console.log("-");
+//     }
+//     else{
+//         console.log("*");
+//     }
+// a++;
+// }
+// //3
+// for (let k = 0; k < 10; k++) {
+//     for (let l = 1; l <= 10; l++) {
+//       console.log(l);
+//     }
+// }
+// //4
+// for (let a = 100; a <= 200; a++) {
+//     console.log(`a = ${a}`);
+// }
+
+// for (let a = 100; a <= 200; a++) {
+//     if (a % 2 == 0) {
+//       console.log("-");
+//     } else {
+//       console.log("*");
+//     }
+//   }
+// }
+// //5
+
+// let day = 0;
+// switch(day){
+//     case 1: 
+//     console.log('monday');
+
+//     case 2:
+//     console.log('tuesday');
+
+//     case 3:
+//     console.log('wednesday');
+//     break;
+
+//     case 4:
+//     console.log('thursday');
+
+//     case 5:
+//     console.log('friday');
+
+//     case 6:
+//     console.log('saturday');
+
+//     case 7:
+//     console.log('sunday');
+//     break;
+// }
+
+//QA excercise 
+// let strictA = true;
+// let strictB = 1;
+
+let strictA = true;
+let strictB = 1;
+
+console.log(strictA == strictB);
+console.log(strictA === strictB);
+
+console.log(strictA != strictB);
+console.log(strictA !== strictB)
+
+let age = 36;
+
+if (age >=18 && age <=65) {
+    console.log("good age");
+}
+    else if (age < 18) {
+        console.log("too young");
+    }
+else {
+console.log("too mature");
+}
+
+    
+
+
+
+
+
+
